9b51688aaa11a7d7cd604d9c206797cc1c8f5375
sajal-jayanto <sajaljayanto007@gmail.com>
2026-09-26 02:31:08 +0600

    error added

--- src/repository/menuItem.repository.ts
+++ src/repository/menuItem.repository.ts
@@ -1,15 +1,21 @@
 import { Repository } from "typeorm";
 import { MenuItem, MenuItemDto } from "../entities/menu-item.entity.js";
 import { getDataSource } from "../db/typeorm.js";
+import { HttpError } from "../middlewares/error.middleware.js";
+import { StatusCodes } from "http-status-codes";
 
 export class MenuItemRepository {
   private get repo(): Repository<MenuItem> {
     return getDataSource().getRepository(MenuItem);
   }
 
-  async create(payload: MenuItemDto) {
-    const menuItem = await this.repo.create(payload);
-    return this.repo.save(menuItem);
+  async create(payload: MenuItemDto): Promise<MenuItem> {
+    try {
+      const menuItem = this.repo.create(payload);
+      return this.repo.save(menuItem);
+    } catch (error) {
+      throw new HttpError("Failed to create menu item" , StatusCodes.EXPECTATION_FAILED, error as Error);
+    }
   }
 
 }
