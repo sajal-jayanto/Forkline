cf17aa5d5b9734fff3fd87173ec38f6274393d70
sajal-jayanto <sajaljayanto007@gmail.com>
2026-09-26 02:09:02 +0600

    demo test data

--- src/service/menuItem.service.ts
+++ src/service/menuItem.service.ts
@@ -1,7 +1,7 @@
 import { MenuItemDto } from "../entities/menu-item.entity.js";
 import { MenuItemRepository } from "../repository/menuItem.repository.js";
 import { v4 as uuIdv4 } from 'uuid';
-import { demoUrl } from "../utils.js";
+import { demoDescription, demoUrl } from "../utils.js";
 
 export class MenuItemService {
   private menuItemRepository = new MenuItemRepository();
